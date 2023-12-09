acfb69fb003d340309c4844e226fcec48efadf74
Alfredo <alfloz01@ucm.es>
2023-12-09 13:38:57 +0100

    Add beer routes and index and correct sonar

--- src/middleware/file.interceptor.ts
+++ src/middleware/file.interceptor.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+// eslint-disable-next-line no-unused-vars
+import randomUUID from 'crypto';
 import multer from 'multer';
 
 export class FileInterceptor {
