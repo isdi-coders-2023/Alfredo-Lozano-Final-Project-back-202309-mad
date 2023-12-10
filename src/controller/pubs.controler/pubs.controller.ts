5ee611dddd2f2334b3beb9d70158c2bc792bc242
Alfredo <alfloz01@ucm.es>
2023-12-10 12:39:22 +0100

    Create pubs routes

--- src/controller/pubs.controler/pubs.controller.ts
+++ src/controller/pubs.controler/pubs.controller.ts
@@ -1,1 +1,32 @@
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { Controller } from '../controller';
+import { Pubs } from '../../entities/pubs.model';
+import { PubsMongoRepo } from '../../repos/pubs/pubs.mongo.repo';
 
+const debug = createDebug('W9Final:pubs:controller');
+
+export class PubsController extends Controller<Pubs> {
+  constructor(protected repo: PubsMongoRepo) {
+    super(repo);
+    debug('Instantiated');
+  }
+
+  async addBeer(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.repo.addBeer(req.params.id, req.body);
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async removeBeer(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.repo.removeBeer(req.params.id, req.body.userId);
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
--- src/routers/pub.routes.ts
+++ src/routers/pub.routes.ts
@@ -1,1 +1,14 @@
+import { PubsController } from '../controller/pubs.controler/pubs.controller';
+import { PubsMongoRepo } from '../repos/pubs/pubs.mongo.repo';
+import { Router as createRouter } from 'express';
+import createDebug from 'debug';
 
+const debug = createDebug('W9Final:pubs:router');
+
+export const pubsRouter = createRouter();
+debug('Starting');
+
+const repo = new PubsMongoRepo();
+const controller = new PubsController(repo);
+
+pubsRouter.post('/add', controller.create.bind(controller));
