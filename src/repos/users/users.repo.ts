0f31893de7762b85500048ce75c5c04273e821bf
alfredo <alfloz01@ucm.es>
2023-12-18 18:00:46 +0100

    Correct all methods and test

--- src/repos/users/users.repo.ts
+++ src/repos/users/users.repo.ts
@@ -3,12 +3,15 @@ export interface UserRepository<
   Y extends { id: unknown }
 > {
   getAll(): Promise<X[]>;
-  getById(_id: X['id']): Promise<X | null>; // Cambiado a Promise<X | null>
+  getById(_id: X['id']): Promise<X>;
+  // eslint-disable-next-line no-unused-vars
   search({ key, value }: { key: keyof X; value: unknown }): Promise<X[]>;
   create(_newItem: Omit<X, 'id'>): Promise<X>;
   update(_id: X['id'], _updatedItem: Partial<X>): Promise<X>;
   delete(_id: X['id']): Promise<void>;
 
   addBeer(_beer: Y, _userId: X['id']): Promise<X>;
+  // AddPub(_PubId: X['id'], _userId: X['id']): Promise<X>;
   removeBeer(_beer: Y, _userId: X['id']): Promise<X>;
+  // RemovePub(_id: X['id'], _pubIdToRemove: X['id']): Promise<X>;
 }
